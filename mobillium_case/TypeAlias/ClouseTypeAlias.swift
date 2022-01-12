8361325a01d1fd236a08297f25280572750244a2
Sercan KAYA <60138522+SercanKaya0@users.noreply.github.com>
2022-01-12 17:58:27 +0300

    initialize project

---
+++ mobillium_case/TypeAlias/ClouseTypeAlias.swift
@@ -0,0 +1,13 @@
+//
+//  ClouseTypeAlias.swift
+//  public_apis
+//
+//  Created by Sercan KAYA on 10.01.2022.
+//
+
+import Foundation
+public typealias VoidClosure = (() -> Void)
+public typealias IntClosure = ((Int) -> Void)
+public typealias BoolClosure = ((Bool) -> Void)
+public typealias StringClosure = ((String) -> Void)
+public typealias IndexPathClosure = ((IndexPath) -> Void)
---
+++ mobillium_caseUITests/mobillium_caseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mobillium_caseUITestsLaunchTests.swift
+//  mobillium_caseUITests
+//
+//  Created by Sercan KAYA on 12.01.2022.
+//
+
+import XCTest
+
+class mobillium_caseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
